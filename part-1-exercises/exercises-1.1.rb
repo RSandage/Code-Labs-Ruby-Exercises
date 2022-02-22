be48f89287a8573d28d16e03c4d806f8e639babd
Robert Sandage <90716032+RSandage@users.noreply.github.com>
2022-02-22 19:10:44 +0000

    exercises 1.1 and 1.2

---
+++ part-1-exercises/exercises-1.1.rb
@@ -0,0 +1,13 @@
+p "p"
+puts "puts"
+print "print"
+
+my_name = "Robert"
+
+print my_name
+
+beginning_sentence = "My name is"
+
+full_sentence = beginning_sentence + " " + my_name
+
+print full_sentence
---
+++ part-1-exercises/exercises-1.2.rb
@@ -0,0 +1,6 @@
+def say_hello_to(my_name)
+  name = my_name
+  print "Hello," + " " + name
+end
+
+say_hello_to("Robert Sandage")
